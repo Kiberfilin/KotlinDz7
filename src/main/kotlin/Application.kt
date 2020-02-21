67ff23bdae3a73404071eebf660e7de30fd28cc5
Kiberfilin <kiberfilin@gmail.com>
2020-02-21 12:05:07 +0300

    Initial commit

---
+++ src/main/kotlin/Application.kt
@@ -0,0 +1,49 @@
+import io.ktor.application.Application
+import io.ktor.application.call
+import io.ktor.application.install
+import io.ktor.features.ContentNegotiation
+import io.ktor.features.ParameterConversionException
+import io.ktor.features.StatusPages
+import io.ktor.gson.gson
+import io.ktor.http.HttpStatusCode
+import io.ktor.response.respond
+import io.ktor.routing.Routing
+import io.ktor.server.cio.EngineMain
+import io.ktor.util.KtorExperimentalAPI
+import org.kodein.di.generic.bind
+import org.kodein.di.generic.singleton
+import org.kodein.di.ktor.KodeinFeature
+
+fun main(args: Array<String>) {
+    EngineMain.main(args)
+}
+
+@KtorExperimentalAPI
+fun Application.module() {
+    install(ContentNegotiation){
+        gson {
+            setPrettyPrinting()
+            serializeNulls()
+        }
+    }
+
+    install(StatusPages){
+        exception<NotImplementedError> {
+            call.respond(HttpStatusCode.NotImplemented)
+        }
+        exception<ParameterConversionException> {
+            call.respond(HttpStatusCode.BadRequest)
+        }
+        exception<Throwable> {
+            call.respond(HttpStatusCode.InternalServerError)
+        }
+    }
+    install(KodeinFeature) {
+
+        bind<PostRepository>() with singleton { PostRepositoryInMemoryWithMutexImpl() }
+    }
+
+    install(Routing) {
+        v1()
+    }
+}
---
+++ src/main/kotlin/route/v1.kt
@@ -0,0 +1,8 @@
+package route
+
+import io.ktor.routing.Route
+import io.ktor.routing.Routing
+
+fun Routing.v1() {
+    Route("")
+}
